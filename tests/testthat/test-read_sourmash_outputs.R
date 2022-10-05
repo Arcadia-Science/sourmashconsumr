8b5446fc9f5060f9ce5e8fd3bc1731dad89029f8
Taylor Reiter <taylorreiter@gmail.com>
2022-10-05 17:01:36 -0400

    add compare tests and tests data

---
+++ tests/testthat/test-read_sourmash_outputs.R
@@ -0,0 +1,24 @@
+
+# test read_compare_csv --------------------------------------------------------
+
+test_that("check read_compare_csv is symmetric", {
+  df = read_compare_csv("comp_k31.csv", sample_to_rownames = T)
+  expect_equal(ncol(df), nrow(df))
+})
+
+test_that("check read_csv pass through arguments work correctly for read_compare_csv.", {
+  # show_col_types produces a message about info in the tibble.
+  expect_message(read_compare_csv("comp_k31.csv", sample_to_rownames = F, show_col_types = T))
+})
+
+test_that("check that column names are the same as rownames for read_compare_csv", {
+  df = read_compare_csv("comp_k31.csv", sample_to_rownames = T)
+  expect_equal(colnames(df), rownames(df))
+})
+
+
+# test read_taxonomy_annotate ---------------------------------------------
+
+test_that("check that", {
+
+})
