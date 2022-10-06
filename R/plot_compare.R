11b07ddb8c57c9b4a8d8222e67c9e55f020fc2ab
Taylor Reiter <taylorreiter@gmail.com>
2022-10-06 17:42:21 -0400

    add rudimentary code for mds plot

--- R/plot_compare.R
+++ R/plot_compare.R
@@ -1,20 +1,12 @@
-make_compare_mds <- function(){
-
-}
-
-plot_compare_mds <- function(){}
-
-#' Plot a heatmap from a tibble of sourmash compare results.
+#' Internal function to check if the sample name is a column or a rowname and move it to a rowname if it is a column.
 #'
-#' @param compare_df A tibble containing results produced by sourmash.
-#' @param ... Arguments passed to base::heatmap().
+#' @param compare_df
 #'
-#' @return A plot of sourmash compare results.
-#' @export
+#' @return A tibble.
 #'
 #' @examples
-#' plot_compare_heatmap()
-plot_compare_heatmap <- function(compare_df, ...){
+#' check_compare_df_sample_col_and_move_to_rowname(compare_df)
+check_compare_df_sample_col_and_move_to_rowname <- function(compare_df){
   # check if compare tibble was read in with sample names as a column
   if("sample" %in% colnames(compare_df)){
     # guard against missing pkg installations
@@ -28,6 +20,83 @@ plot_compare_heatmap <- function(compare_df, ...){
     compare_df <- compare_df %>%
       tibble::column_to_rownames("sample")
   }
+  return(compare_df)
+}
+
+
+#' Perform an MDS analysis from a sourmash compare output and produce a tidy data.frame
+#'
+#' @param compare_df
+#'
+#' @return A data.frame.
+#' @export
+#'
+#' @examples
+make_compare_mds <- function(compare_df){
+  # guard against missing pkg installations
+  if (!requireNamespace("tibble", quietly = TRUE)) {
+    stop(
+      "Package \"tibble\" must be installed to run make_compare_mds(). Please install \"tibble\".",
+      call. = FALSE
+    )
+  }
+
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop(
+      "Package \"dplyr\" must be installed to run make_compare_mds(). Please install \"dplyr\".",
+      call. = FALSE
+    )
+  }
+
+  # check if compare tibble was read in with sample names as a column
+  compare_df <- check_compare_df_sample_col_and_move_to_rowname(compare_df)
+
+  compare_mds <- compare_df %>%
+    as.matrix() %>%
+    dist() %>%
+    cmdscale() %>%
+    as.data.frame() %>%
+    dplyr::rename(MDS1 = V1, MDS2 = V2) %>%
+    tibble::rownames_to_column("sample")
+
+  return(compare_mds)
+}
+
+#' Plot an MDS data.frame produces from the output of sourmash compare
+#'
+#' @param compare_mds A data.frame produced using make_compare_mds()
+#'
+#' @return A ggplot2 plot.
+#' @export
+#'
+#' @examples
+plot_compare_mds <- function(compare_mds){
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop(
+      "Package \"ggplot2\" must be installed to make an MDS plot. Please install \"ggplot2\".",
+      call. = FALSE
+    )
+  }
+
+  ggplot2::ggplot(compare_mds, ggplot2::aes(x = MDS1, y = MDS2, label = sample)) +
+    ggplot2::geom_point() +
+    #geom_label_repel() +
+    ggplot2::theme_classic()
+}
+
+#' Plot a heatmap from a tibble of sourmash compare results.
+#'
+#' @param compare_df A tibble containing results produced by sourmash.
+#' @param ... Arguments passed to base::heatmap().
+#'
+#' @return A plot of sourmash compare results.
+#' @export
+#'
+#' @examples
+#' plot_compare_heatmap()
+plot_compare_heatmap <- function(compare_df, ...){
+  # check if compare tibble was read in with sample names as a column
+  compare_df <- check_compare_df_sample_col_and_move_to_rowname(compare_df)
 
   compare_mat <- as.matrix(compare_df)
 
