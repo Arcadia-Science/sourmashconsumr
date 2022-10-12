638f03623436f6c9bcf2b26db95cb597c4da55e6
Taylor Reiter <taylorreiter@gmail.com>
2022-10-12 15:24:26 -0400

    add tests for make mds function

--- tests/testthat/test-plot_compare.R
+++ tests/testthat/test-plot_compare.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("make_compare_mds outputs a data frame of the correct shape", {
+  mds <- make_compare_mds(read_compare_csv("tests/testthat/comp_k31.csv"))
+  expect_equal(ncol(mds), 3)
+  expect_equal(nrow(mds), 6)
+})
+
+test_that("make_compare_mds calculates the correct position for one sample", {
+  mds <- make_compare_mds(read_compare_csv("tests/testthat/comp_k31.csv"))
+  expect_equal(mds[1, 2], -0.37079668)
+  expect_equal(mds[1, 3], -0.26357615)
 })
