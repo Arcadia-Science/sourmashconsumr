a3c8b202241ca7703aa23489f3f12d4235f4ec5c
Taylor Reiter <taylorreiter@gmail.com>
2022-10-20 08:47:49 -0400

    export from stats

--- R/plot_compare.R
+++ R/plot_compare.R
@@ -2,12 +2,12 @@
 #'
 #' @param compare_df
 #'
-#' @return A tibble.
+#' @return A data frame.
 #'
 #' @examples
 #' check_compare_df_sample_col_and_move_to_rowname(compare_df)
 check_compare_df_sample_col_and_move_to_rowname <- function(compare_df){
-  # check if compare tibble was read in with sample names as a column
+  # check if compare data frame was read in with sample names as a column
   if("sample" %in% colnames(compare_df)){
     # move sample column to rownames
     compare_df <- compare_df %>%
@@ -19,9 +19,9 @@ check_compare_df_sample_col_and_move_to_rowname <- function(compare_df){
 
 #' Perform an MDS analysis from a sourmash compare output and produce a tidy data.frame
 #'
-#' @param compare_df A tibble or data.frame produced by read_compare_csv() representing a (dis)similarity matrix output by sourmash compare.
+#' @param compare_df A data frame produced by read_compare_csv() representing a (dis)similarity matrix output by sourmash compare.
 #'
-#' @return A data.frame.
+#' @return A data frame.
 #' @export
 #'
 #' @examples
@@ -32,8 +32,8 @@ make_compare_mds <- function(compare_df){
 
   compare_mds <- compare_df %>%
     as.matrix() %>%
-    dist() %>%
-    cmdscale() %>%
+    stats::dist() %>%
+    stats::cmdscale() %>%
     as.data.frame() %>%
     dplyr::rename(MDS1 = V1, MDS2 = V2) %>%
     tibble::rownames_to_column("sample")
@@ -44,7 +44,7 @@ make_compare_mds <- function(compare_df){
 #' Plot an MDS data.frame produced from the output of sourmash compare
 #'
 #' @param compare_mds A data.frame produced using make_compare_mds()
-#' @param label Boolean controlling whether sample labels are added to the plot. Default TRUE plots labels.
+#' @param label Boolean controlling whether sample labels are added to the plot. The default, TRUE, plots labels.
 #'
 #' @return A ggplot2 plot.
 #' @export
@@ -67,6 +67,7 @@ plot_compare_mds <- function(compare_mds, label = TRUE){
 #' Plot a heatmap from a tibble of sourmash compare results.
 #'
 #' @param compare_df A tibble or data.frame produced by read_compare_csv() representing a (dis)similarity matrix output by sourmash compare.
+#' @param seed Random seed for function. Default is 42. Ensures repeatable plot outputs.
 #' @param ... Arguments passed to base::heatmap().
 #'
 #' @return A plot of sourmash compare results.
@@ -84,13 +85,12 @@ plot_compare_heatmap <- function(compare_df, seed = 42, ...){
   compare_mat <- as.matrix(compare_df)
 
   # use hclust() and dist() to calculate clusters
-  hc_rows <- hclust(dist(compare_mat), method = "single") # method single matches sourmash plot
-  hc_cols <- hclust(dist(t(compare_mat)), method = "single")
+  hc_rows <- stats::hclust(stats::dist(compare_mat), method = "single") # method single matches sourmash plot
+  hc_cols <- stats::hclust(stats::dist(t(compare_mat)), method = "single")
 
   # plot the heatmap
-  heatmap(compare_mat,
-          Colv = as.dendrogram(hc_cols),
-          Rowv = as.dendrogram(hc_rows),
-          scale='none', ...)
+  stats::heatmap(compare_mat,
+                 Colv = stats::as.dendrogram(hc_cols),
+                 Rowv = stats::as.dendrogram(hc_rows),
+                 scale = 'none', ...)
 }
-
